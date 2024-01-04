9455ddade4e1481703d80ee1ff8fa4c5ca4d1844
chuacongtai <ngothanhtai12345a@gmail.com>
2024-01-04 10:40:21 +0700

    update lesson6

---
+++ Lesson6/Ex1/App.xaml.cs
@@ -0,0 +1,26 @@
+﻿using Ex1.View_Model;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Ex1
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            Ex1.View.MainPage window = new Ex1.View.MainPage();
+            UserViewModel VM = new UserViewModel();
+            window.DataContext = VM;
+            window.Show();
+        }
+    }
+}
